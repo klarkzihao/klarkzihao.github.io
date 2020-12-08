93bad0997472de07cefa93e06bc3d94c215b723b
klarkzihao <klarkzh@gmail.com>
2020-12-08 15:50:15 +0800

    add chunk

--- miao/lodash/klarkzihao-lodash.js
+++ miao/lodash/klarkzihao-lodash.js
@@ -1,4 +1,12 @@
 var klarkzihao = function () {
+  function chunk(ary, size = 1) {
+    var result = []
+    for (let i = 0; i < ary.length; i += size) {
+      result.push(ary.slice(i, i + size))
+    }
+    return result
+  }
+
   function compact(ary) {
     for (let i = 0; i < ary.length; i++) {
       if (!ary[i] || ary[i] == 0) {
